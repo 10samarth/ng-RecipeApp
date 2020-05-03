4ae6ed9c7c8a2a842fa7949b23d2e827f7c33edb
samarthg95 <samarthmg@gmail.com>
2020-05-03 18:29:31 +0530

    2nd stage

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe','Test Description','https://previews.123rf.com/images/csstockimages/csstockimages1710/csstockimages171001097/88101444-indian-traditional-street-food-pav-bhaji.jpg'),
+    new Recipe('A Test Recipe 2','Test Description 2','https://previews.123rf.com/images/csstockimages/csstockimages1710/csstockimages171001097/88101444-indian-traditional-street-food-pav-bhaji.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
